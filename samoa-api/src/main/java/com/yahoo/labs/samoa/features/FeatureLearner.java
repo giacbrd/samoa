a2a7011a7f2fb2627948d26ec99afda7bc0ec7ce
giacbrd <Ber4com0>
2014-10-09 17:42:16 +0200

    Feature learner interface

---
+++ samoa-api/src/main/java/com/yahoo/labs/samoa/features/FeatureLearner.java
@@ -0,0 +1,31 @@
+package com.yahoo.labs.samoa.features;
+
+/*
+ * #%L
+ * SAMOA
+ * %%
+ * Copyright (C) 2013 - 2014 Yahoo! Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+/**
+ * A feature learner generates features from a input stream
+ * and it produces a data samples stream for learners (classifiers).
+ *
+ * @author Giacomo Berardi <barnets@gmail.com>.
+ */
+public interface FeatureLearner {
+}
